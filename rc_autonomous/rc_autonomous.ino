afdc17ed4d97e2de56fe942da6a9b235561788e9
Igz <you@example.com>
2017-07-12 21:11:05 -0400

    changing backup logic..

--- rc_autonomous/rc_autonomous.ino
+++ rc_autonomous/rc_autonomous.ino
@@ -21,18 +21,15 @@ int angle = 0;    // variable to store the servo position
 int delta = 20;   // new increment for the servo adjustment, move by this value
 //-------------------------------------------
 
-const int distanceArrayMaxSize = 180 / 20 + 1; //180 degrees divided by delta + 0th position
-//int maxDistance = 0; // greatest distance seen thus far
-int dArray[distanceArrayMaxSize]; //array of distances, indexed by angles
-
-unsigned long previousMillis = 0; //will store last time the servo was moved
-
-const long interval = 50;        // interval at which to reposition the servo
+const int distanceArrayMaxSize = 180 / 20 + 1;  // 180 degrees divided by delta + 0th position
+int dArray[distanceArrayMaxSize];               // array of distances, indexed by angles
+unsigned long previousMillis = 0;               // will store last time the servo was moved
+const long interval = 50;                       // interval in milliseconds at which to reposition the servo
 
 void setup() {
 
-  radarServo.attach(10); //pin 10
-  Serial.begin(9600);           // set up Serial library at 9600 bps
+  radarServo.attach(10);                        //pin 10
+  Serial.begin(9600);                           // set up Serial library at 9600 bps
 
   Wire.begin();
 
@@ -46,9 +43,9 @@ void setup() {
   AFMS.begin();  // create with the default frequency 1.6KHz
 
   //init the array to all 0s
-  //  for (int i=0; i<distanceArrayMaxSize; i++) {
-  //    dArray[i] = 0;
-  //  }
+  for (int i = 0; i < distanceArrayMaxSize; i++) {
+    dArray[i] = 0;
+  }
 
 }
 
@@ -71,21 +68,16 @@ void loop() {
 
 
   //what as the servo's position when this new reading happened? Map it to the steering value.
-  //if less than 90, turn left. If greater than 90, turn right.
-
   //NOTE: the servo is upside down, the values are reversed!
   //IOW, it goes from 180 on the left to 0 on the right.
-
   if (optimalAngle < 90) { //should we turn right?
     steeringMotor->run(FORWARD); //turn the wheels right. FORWARD=right
-    turn = map(optimalAngle, 0, 90, 180, 0);
-    //turn = 4.5 * optimalAngle - 180;
+    turn = map(optimalAngle, 0, 90, 150, 0);
     steeringMotor->setSpeed(turn);
     //Serial.println("RIGHT TURN");
   } else {
     steeringMotor->run(BACKWARD); //turn the wheels left
-    turn = map(optimalAngle, 90, 180, 0, 180);
-    //turn = -4.5 * optimalAngle + 180;
+    turn = map(optimalAngle, 90, 180, 0, 150);
     steeringMotor->setSpeed(turn);
     //Serial.println("LEFT TURN");
   }
@@ -95,30 +87,20 @@ void loop() {
     Serial.print(" TIMEOUT");
   }
 
-  /*
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.print(" Current angle: ");
-    Serial.print(angle);
-    Serial.print(" Best Angle: ");
-    Serial.print(optimalAngle);
-    Serial.print(" index: ");
-    Serial.print(index);
-    Serial.print(" Delta: ");
-    Serial.print(delta);
-    Serial.print("  Turn radius: ");
-    Serial.println(turn);
-  */
-
-  speed = map(distance, 10, 1100, 0, 150);
+  //speed = map(distance, 10, 1100, 0, 80);
+  speed = 80;
   goForward(speed);
 
-  //backup if too close to an obstacle
-  while (distance < 100) {
+  //backup if too close to an obstacle but only if within the narrow band
+  //between 80 and 100 degrees
+  while (distance < 100 && angle < 100 && angle > 80) {
     steeringMotor->run(BACKWARD);
-    steeringMotor->setSpeed(200);
+    steeringMotor->setSpeed(130);
+    Serial.println("Go backwards! ");
     goBackward();
     distance = frontSensor.readRangeContinuousMillimeters();
+    Serial.print("angle: ");
+    Serial.println(angle);
   }
 
 
@@ -130,13 +112,15 @@ void loop() {
   unsigned long currentMillis = millis();
 
   if ((unsigned long)(currentMillis - previousMillis) >= interval) {
-    angle += delta;
-    // save the last time we moved
-    previousMillis = currentMillis;
-    radarServo.write(angle);              // tell servo to go to position in variable 'pos'
 
-    if ((angle == 0) || (angle == 180)) {
-      delta = -delta;
+    // increment the servo angle by 'delta'
+    // if delta is negative, angle will decrease.
+    angle += delta;
+    previousMillis = currentMillis;       // save the last time we moved
+    radarServo.write(angle);              // tell servo to go to position 'angle'
+
+    if ((angle == 0) || (angle == 180)) { //are we at extreme left or right?
+      delta = -delta;                     // yes, reverse the delta
       //delay(5000);
     }
 
@@ -163,6 +147,10 @@ void goBackward() {
   rearMotor->setSpeed(bwdSpeed);
 
   //  delay(1000);
+}
+
+void goStraight() {
+  steeringMotor->setSpeed(0);
 }
 
 
